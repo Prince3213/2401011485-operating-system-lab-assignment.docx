00079b2890fd4fd06324804eff8c4cbc78f6a50f
Prince3213 <kumarguptaprince215@gmail.com>
2025-11-02 19:14:59 +0530

    Delete task3.py

--- task3.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-if __name__ == '__main__':
-    print("System Starting...")
-    # Create processes
-    p1 = multiprocessing.Process(target=system_process, args=('Process-1',))
-    p2 = multiprocessing.Process(target=system_process, args=('Process-2',))
-    # Start processes
-    p1.start()
-    p2.start()
