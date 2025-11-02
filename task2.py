f8a8d1e3ff06bbeca779a563d26db1b19eaaac7b
Prince3213 <kumarguptaprince215@gmail.com>
2025-11-02 19:16:33 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,6 +0,0 @@
-import time
-# Dummy function to simulate a task
-def system_process(task_name):
-    logging.info(f"{task_name} started")
-    time.sleep(2)  # Simulate task delay
-    logging.info(f"{task_name} ended")
